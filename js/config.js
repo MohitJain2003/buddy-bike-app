f454cda6013c2e82395f7a1c332ef46152d438f2
MohitJain2003 <160161496+MohitJain2003@users.noreply.github.com>
2026-05-02 12:44:02 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// Configuration file to hold environment-like variables
-const CONFIG = {
-    SUPABASE_URL: "https://vyosnvoqqdreonojihcm.supabase.co",
-    SUPABASE_KEY: "sb_publishable_wW_mJdQCU3ErplTFPqH5Nw_ZoXMui63"
-};
